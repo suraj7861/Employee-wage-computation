885429588cf38abc16bbdd116026771ef4d4d2f9
unknown <khomanesuraj8@gmail.com>
2021-09-29 14:26:23 +0530

    Check Employee Attendence

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,12 +1,22 @@
-
 public class EmpWageComputation{
 
 	public static void main(String[] args){
 
-          System.out.println("Welcome to Employee Wage Computation");
+            System.out.println("Welcome to Employee Wage Computation");
+	  
+            final int Emp_Check = 1;
 
-      }
+            double checkAttendence = Math.floor(Math.random() * 10) % 2 ;
+            System.out.println("Random value for emp Attendence check is: "+ checkAttendence);
 
+            if(Emp_Check == checkAttendence){
+                System.out.println("Employee is present");
+               }
+           else{
+                System.out.println("Employee is Absent");	
 
-
+               }
+     }
 }
+ 
+
