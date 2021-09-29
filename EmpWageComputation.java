d49d704475f1880bf6ea4989aa758a997b04e7bf
unknown <khomanesuraj8@gmail.com>
2021-09-29 15:18:54 +0530

    Added Emp Daily Wage is

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,12 +5,15 @@ public class EmpWageComputation{
             System.out.println("Welcome to Employee Wage Computation");
 	  
             final int Emp_Check = 1;
-
+            int Emp_WagePerHr = 20;
+	    int FullDay_Hr = 8;    
             double checkAttendence = Math.floor(Math.random() * 10) % 2 ;
             System.out.println("Random value for emp Attendence check is: "+ checkAttendence);
 
             if(Emp_Check == checkAttendence){
+                int income = Emp_WagePerHr * FullDay_Hr;
                 System.out.println("Employee is present");
+	        System.out.println("Employee Daily wage is: "+ income);
                }
            else{
                 System.out.println("Employee is Absent");	
