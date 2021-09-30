af83f4fd24c3545308272e2542ec816335ae0cbf
unknown <khomanesuraj8@gmail.com>
2021-09-30 12:21:23 +0530

    Added changes in swith case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,48 +1,37 @@
 import java.util.Random;
 
 public class EmpWageComputation{
+       public static final int Is_Full_Time = 1;
+       public static final int Is_Part_Time = 2;
+       public static final int Emp_WagePerHr = 20;
 
 	public static void main(String[] args){
 
-            System.out.println("Welcome to Employee Wage Computation");
-	  
-            final int Emp_Check = 1;
-	    int Is_Full_Time = 1;
-	    int Is_Part_Part = 0;
-            int Emp_WagePerHr = 20;
-	    int FullDay_Hr = 8;
-	    int PartTime_Hr = 4;
-	    int income = 0;
+	      System.out.println("Welcome to Employee Wage Computation");
+
+	    int empHrs = 0;
+	    int empWage = 0;    
+            int Emp_Check  = (int) Math.floor(Math.random() * 10) % 3 ;
+            System.out.println("Random value for emp Attendence check is: "+ Emp_Check);
+
+            switch (Emp_Check){
+                case Is_Full_Time:
+                	System.out.println("Employee is present Full Time");
+	        	empHrs = 8;
+               		break;
                
-           
-            Random rand = new Random();
-            int m = rand.nextInt(3);
-             //double n = Math.floor(Math.random() * 10) % 2 ;
-	    
-            switch(m){
-
-	       case 1:
-	             
-                       income = Emp_WagePerHr * FullDay_Hr;
-                       System.out.println("Employee is present And Full Time Work");
-	               System.out.println("Employee Daily wage is: "+ income);
-	        	break;
-
-	          
-	        case 0:
-
-	                income = Emp_WagePerHr * PartTime_Hr;
-                        System.out.println("Employee is present And Part Time Work");
-                        System.out.println("Employee Daily wage is: "+ income);
-		        break;
-
-			    
+	        case Is_Part_Time:
+               		System.out.println("Employee is present Part Time");
+	        	empHrs = 4;
+			break;
+          
                 default:
-                 	System.out.println("Employee is Absent and income is:"+ income);
-	                break;
-                 
-                  }
-     
+                	System.out.println("Employee is Absent");
+                	empHrs = 0;	
+               }
+	    empWage = empHrs * Emp_WagePerHr;
+	    System.out.println("Employee Wage is; "+ empWage);  
+
        }
  
 }
