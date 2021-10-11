fc77f1052bf4d8d00d0d851a904f96e7ca08ce5f
unknown <khomanesuraj8@gmail.com>
2021-10-11 18:50:40 +0530

    merge UC6 to master

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -11,21 +11,7 @@ public class EmpWageComputation{
        public static void main(String[] args){
 	
             System.out.println("Welcome to Employee Wage Computation");
-
-	    int empHrs = 0, totalEmpHrs = 0, TotalWorkingDays = 1;
-
-	    //while loop for check Total emp hours is less than 100 and Total working days less than 20
-	    while (totalEmpHrs <= Max_Hrs_In_Month && 
-                    TotalWorkingDays <= Num_Working_Days){
-
-                    TotalWorkingDays++ ;      
-		    //type casting here to get integer value
-	            int Emp_Check  = (int) Math.floor(Math.random() * 10) % 3 ;
-            	    // use swith case to choose 0,1,2
-        	    switch (Emp_Check){
-               		case Is_Full_Time:
-
-
+	  
 	    int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 1;
 	    
 	    //check total emp hour not more than 100 and total days not more than 20
