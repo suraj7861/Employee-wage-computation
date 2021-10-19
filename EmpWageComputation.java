3a5ccdb94e0e83f326063885b8261b872ce53b03
unknown <khomanesuraj8@gmail.com>
2021-10-19 21:02:53 +0530

    Added Emp Wage for multiple companies

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -11,22 +11,22 @@ public class EmpWageComputation{
 
     // Default constructor: EmpWageComputation class and array initialization
     public EmpWageComputation() {
-            companyWageArray = new CompanyEmpWage[5];
+        companyWageArray = new CompanyEmpWage[5];
     }
 
     // Add companyEmpWage method: add company and passing parameter list
-	private void addComapanyEmpWage(String company, int empWagePerHr, int workingDayPerMonth, int maxHrsInMonth) {
+    private void addComapanyEmpWage(String company, int empWagePerHr, int workingDayPerMonth, int maxHrsInMonth) {
         companyWageArray[noOfCompany] = new CompanyEmpWage(company, empWagePerHr, workingDayPerMonth, maxHrsInMonth);
         noOfCompany++;
 		
-	}
+    }
 
     //display array
     private void empWageCalculation() {
-            for (int i = 0; i < noOfCompany; i++) {
-                    companyWageArray[i].setTotalEmpWage(empWageCalculation(companyWageArray[i]));
-                    System.out.println(companyWageArray[i]);
-            }
+        for (int i = 0; i < noOfCompany; i++) {
+            companyWageArray[i].setTotalEmpWage(empWageCalculation(companyWageArray[i]));
+            System.out.println(companyWageArray[i]);
+        }
     }
 
     //method: employee wage calculation
@@ -36,35 +36,35 @@ public class EmpWageComputation{
 
         //check total emp hour not more than 100 and total days not more than 20
         while (totalEmpHrs <= companyEmpWage.maxHrsInMonth &&
-               totalWorkingDays <= companyEmpWage.workingDayPerMonth){
+            totalWorkingDays <= companyEmpWage.workingDayPerMonth){
 
-                //random number for employee check
-                int emp_Check  = (int) Math.floor(Math.random() * 10) % 3 ;
+            //random number for employee check
+            int emp_Check  = (int) Math.floor(Math.random() * 10) % 3 ;
 
-                //switch case to select emp full time, part time or absent
-                switch (emp_Check){
-                    case IS_FUll_TIME:
-                            System.out.println("Employee is present Full Time");
-                            empHrs = 8;
-                            break;
+            //switch case to select emp full time, part time or absent
+            switch (emp_Check){
+            case IS_FUll_TIME:
+                System.out.println("Employee is present Full Time");
+                empHrs = 8;
+                break;
 
-                    case IS_PART_TIME:
-                            System.out.println("Employee is present Part Time");
-                            empHrs = 4;
-                            break;
+            case IS_PART_TIME:
+                System.out.println("Employee is present Part Time");
+                empHrs = 4;
+                break;
 
-                    default:
-                            System.out.println("Employee is Absent");
-                            empHrs = 0;
-                    }
+            default:
+                System.out.println("Employee is Absent");
+                empHrs = 0;
+            }
 
-             //calculate total emp hours
-             totalEmpHrs += empHrs;
-             System.out.println("Day : "+ totalWorkingDays +" Emp Hrs : "+empHrs);
+            //calculate total emp hours
+            totalEmpHrs += empHrs;
+            System.out.println("Day : "+ totalWorkingDays +" Emp Hrs : "+empHrs);
 
             //day increment by 1 every iteration
-             totalWorkingDays++ ;
-           }
+            totalWorkingDays++ ;
+        }
 
         // calculate total emp wage
         return companyEmpWage.totalEmpWage = totalEmpHrs * companyEmpWage.empWagePerHr;
@@ -80,8 +80,6 @@ public class EmpWageComputation{
         companyWageArray.addComapanyEmpWage("Jio",25,2,10);
         companyWageArray.addComapanyEmpWage("Amazon",15,4,10);
         companyWageArray.empWageCalculation();
-
-
     }
 
 }
